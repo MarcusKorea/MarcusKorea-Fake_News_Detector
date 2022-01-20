3a5f7429d24700a53d1ec29dcbbebd9f39bd9c3f
ivana-stopka <ivana_stopka@hotmail.com>
2022-01-20 23:08:44 +1100

    updated index.html, app.py and added model files

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 # Import the required libraries
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from joblib import load
+import pickle
 
 # Load the pipeline object
-pipeline = load("logistic_Regression_Model.joblib")
+#vectorizor = load("vectorizor.joblib")
+#model = load("logistic_Regression_Model.joblib")
+
+#Loading Naive Bayes model
+vectorizor = pickle.load(open("vect.pkl", 'rb'))
+model = pickle.load(open("model.pkl", 'rb'))
 
 # Start Flask
 app =Flask(__name__)
@@ -11,15 +17,30 @@ app =Flask(__name__)
 # home page
 @app.route("/",  methods=["GET","POST"])
 def index():
-    if flask.request.method == "GET":
-        return(flask.render_template("index.html"))
+    if request.method == "GET":
+        return(render_template("index.html"))
 
-    if flask.request.method == "POST":
-        article = flask.request.form["Contents"]
+    if request.method == "POST":
+        article = request.form["Contents"]
+        print(article)
 
-        prediction = pipeline.predict(article)[0]
+        #prediction = pipeline.predict(article)[0]
 
-    return render_template("index.html",result=prediction)
+    return render_template("index.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    print("hohohohohoho")
+    if request.method == 'POST':
+        message = [str(x) for x in request.form.values()][0]
+        #data = [message]
+        print(message)
+        print("***************************")
+        vect = vectorizor.transform(['hello my name is Ivana','nice to meet you']).toarray()
+        print(vect.shape)
+        prediction = model.predict([vect])
+        print(prediction)
+    return render_template('index.html')
 
 # main
 if __name__ == "__main__":
