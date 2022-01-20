ff227f6475c0a621cdeacc73d3df4c3d6d298068
ivana-stopka <ivana_stopka@hotmail.com>
2022-01-20 21:34:31 +1100

    updated app.py and index.html

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
+# Import the required libraries
 from flask import Flask, render_template, redirect
+from joblib import load
 
+# Load the pipeline object
+pipeline = load("logistic_Regression_Model.joblib")
+
+# Start Flask
 app =Flask(__name__)
 
-# db = {connection goes here}
-
-
 # home page
-@app.route("/")
+@app.route("/",  methods=["GET","POST"])
 def index():
-    return "This is the home page"
+    if flask.request.method == "GET":
+        return(flask.render_template("index.html"))
 
-    # return render_template("index.html")
+    if flask.request.method == "POST":
+        article = flask.request.form["Contents"]
+
+        prediction = pipeline.predict(article)[0]
+
+    return render_template("index.html",result=prediction)
 
 # main
 if __name__ == "__main__":
