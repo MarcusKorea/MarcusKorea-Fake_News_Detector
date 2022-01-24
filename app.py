f637b932caa5f152e681ea76972fbe6c10403f6e
MarcusKorea <marcus.a.correia@gmail.com>
2022-01-24 14:23:41 +1100

    fixed files and requirements.txt

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 # Import the required libraries
 from flask import Flask, render_template, redirect, request
-import spacy
-import nltk
-from nltk.tokenize import word_tokenize
 import string
 import pickle
 import os
@@ -11,38 +8,6 @@ port = int(os.environ.get("PORT", 5000))
 #Loading vectoriser
 vectorizor = pickle.load( open("Model_files/Vectorisors/log_reg_vec.pkl","rb"))
 model = pickle.load(open("Model_files/Models/log_reg_model.pkl", 'rb'))
-
-
-# define some user functions
-# remove non ascii characters
-def remove_non_ascii(word):
-    new_word = word.encode("ascii","ignore").decode()
-    return new_word
-    # remove punctuation and stop words
-def clean_input(input):
-    # load language model
-    sp = spacy.load('en_core_web_sm')
-    # import stop words
-    all_stopwords = sp.Defaults.stop_words
-    # import puncuation
-    punc  = string.punctuation
-
-    # replace any weird characters
-    text = input
-    remove_non_ascii(text)
-
-    # remove puncuation
-    new_text = text.translate(str.maketrans('', '', punc))
-
-    # tokenize and make everything lower case
-    text_tokens = word_tokenize(new_text.lower())
-
-    # remove stop words
-    tokens_without_sw= [word for word in text_tokens if not word in all_stopwords]
-
-    # make list a string again
-    tokens_without_sw = " ".join(tokens_without_sw)
-    return tokens_without_sw
 
 # Start Flask
 app =Flask(__name__)
@@ -65,8 +30,6 @@ def predict():
     print("in predict")
     if request.method == 'POST':
         message = [str(x) for x in request.form.values()][0]
-        # removes stop word, punctuation and non ascci characters from entered line
-        message = clean_input(message)
         data = [message]
 
         # feeds entered line into the model
